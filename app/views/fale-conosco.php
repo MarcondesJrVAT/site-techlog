0073c408b862a0a6eb4c54c9e56da12d24cb5083
MarcondesJrVAT <marcondes.junior@vat.com.br>
2025-06-13 10:58:40 -0400

    Atualiza seção "Fale Conosco" com novo formulário de contato e informações de atendimento

--- app/views/fale-conosco.php
+++ app/views/fale-conosco.php
@@ -1,44 +1,74 @@
-            <section class="subpage-header">
-                <div class="container">
-                    <div class="site-title clearfix">
-                        <h2>Fale Conosco</h2>
-                        <ul class="breadcrumbs">
-                            <li><a href="/">Início</a></li>
-                            <li>Fale Conosco</li>
-                        </ul>
+<section class="subpage-header" style="margin-top: 0%;">
+    <div class="container">
+        <div class="site-title clearfix">
+            <h2>Fale Conosco</h2>
+            <ul class="breadcrumbs">
+                <li><a href="/">Início</a></li>
+                <li>Fale Conosco</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row margin-bottom-20">
+            <div class="col-md-12">
+                <?= getMessageSystem(); ?>
+            </div>
+        </div>
+        <div class="row">
+            <!-- Informações de contato -->
+            <div class="col-md-6 col-sm-6 animate fadeInLeft">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 animate fadeInLeft">
+                        <h3>Fale Conosco</h3>
+                        <p>Para entrar em contato com a Techlog, mandar comentários, sugestões ou críticas, utilize o formulário ao lado. Se preferir, utilize os nossos canais de atendimento.</p>
                     </div>
                 </div>
-            </section>
-            <section>
-                <div class="container">
-                    <div class="row margin-bottom-20">
-                        <div class="col-md-12">
-                            <?= getMessageSystem(); ?>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 col-md-offset-3 animate fadeInLeft">
-                            <div class="row">
-                                <div class="col-md-12 col-sm-12 animate fadeInLeft">
-                                    <h3>Fale Conosco</h3>
-                                    <p>Para entrar em contato com o Grupo VAT, mandar comentários, sugestões ou críticas, utilize o formulário ao lado. Se preferir, utilize os nossos canais de atendimento.</p>
-                                </div>
-                            </div>
-                            <div class="height-20"></div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <p><strong>Comercial</strong></p>
-                                    <p><i class="icon-telephone114"></i><a href="tel:+5592985291529">+55 92 98529-1529</a></p>
-                                    <p><i class="icon-envelope"></i> <a href="mailto:comercial@vat.com.br">comercial@vat.com.br</a></p>
-                                </div>
-                                <div class="col-md-6">
-                                    <p><strong>Suporte</strong></p>
-                                    <p><i class="icon-telephone114"></i> <a href="tel:08000464632">0800 046 4632</a></p>
-                                    <p><i class="icon-envelope"></i> <a href="mailto:suporte@vat.com.br">suporte@vat.com.br</a></p>
-                                </div>
-                            </div>
-                        </div>
+                <div class="height-20"></div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <p><strong>Comercial</strong></p>
+                        <p><i class="icon-telephone114"></i><a href="#">(92) 3306-4410</a></p>
+                        <p><i class="icon-telephone114"></i><a href="#">(92) 3306-4405</a></p>
+                        <p><i class="icon-envelope"></i> <a href="mailto:comercial@techlog.com.br">comercial@techlog.com.br</a></p>
                     </div>
                 </div>
-            </section>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15936.383674033894!2d-59.9935868!3d-3.0690373!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x25a1a37b041219c!2sVat%20S%2FA!5e0!3m2!1spt-BR!2sbr!4v1615427207384!5m2!1spt-BR!2sbr" height="450" style="border:0;width:100%" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+
+            <!-- Formulário de contato -->
+            <div class="col-md-6 col-sm-6 animate fadeInRight">
+                <div class="contact-form">
+                    <h3>Envie sua mensagem</h3>
+                    <form action="/enviar-contato.php" method="post">
+                        <div class="form-group">
+                            <label for="name">Nome Completo *</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">E-mail *</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="subject">Assunto *</label>
+                            <select class="form-control" id="subject" name="subject" required>
+                                <option value="">Selecione um assunto</option>
+                                <option value="Comercial">Comercial</option>
+                                <option value="Suporte">Suporte</option>
+                                <option value="Dúvidas">Dúvidas</option>
+                                <option value="Sugestões">Sugestões</option>
+                                <option value="Outros">Outros</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="message">Mensagem *</label>
+                            <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+                        </div>
+                        <button type="submit" class="btn" style="background-color: #e77242; border-color: #d05e2a; color: #fff;">Enviar Mensagem</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.938271497475!2d-59.98240012410699!3d-3.1110349403451663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c052596b9d129%3A0xb61533c92f2ec78f!2sVAT%20Tecnologia%20da%20Informa%C3%A7%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1749824349576!5m2!1spt-BR!2sbr" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"></iframe>
