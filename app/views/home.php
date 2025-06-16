565d73a3b674492fc2b8b7e1a4e601ec3131180f
MarcondesJrVAT <marcondes.junior@vat.com.br>
2025-06-16 07:32:55 -0400

    Refatora estilos e estrutura da seção "Valores" para melhor apresentação e legibilidade

--- app/views/home.php
+++ app/views/home.php
@@ -1,107 +1,129 @@
-        <style>
-            @media (max-width: 450px) {
-                .cd-hero-slider .solutions-video {
-                    top: 27%;
-                }
-            }
+<style>
+    /* Media queries responsivas */
+    @media (max-width: 450px) {
+        .cd-hero-slider .solutions-video {
+            top: 27%;
+        }
+    }
 
-            @media only screen and (min-width:768px) {
-                .embed-responsive-16by9-video {
-                    padding-bottom: 32% !important;
-                }
+    @media only screen and (min-width:768px) {
+        .embed-responsive-16by9-video {
+            padding-bottom: 32% !important;
+        }
 
-                .embed-responsive-item-video {
-                    width: 95% !important;
-                }
+        .embed-responsive-item-video {
+            width: 95% !important;
+        }
+    }
 
-                @media only screen and (max-height: 414px) {
-                    .embed-responsive-16by9-video {
-                        padding-bottom: 60% !important;
-                    }
-                }
-            }
+    @media only screen and (max-height: 414px) {
+        .embed-responsive-16by9-video {
+            padding-bottom: 60% !important;
+        }
+    }
 
-            .valores-list {
-                list-style-type: disc;
-                padding-left: 20px;
-                margin: 0 0 15px 0;
-                color: #444;
-                text-align: justify;
-                word-break: break-word;
-            }
+    /* Estilos gerais padronizados */
+    .valores-list {
+        list-style-type: disc;
+        padding-left: 20px;
+        margin: 0 0 15px 0;
+        color: #444;
+        text-align: justify;
+        word-break: break-word;
+    }
 
-            .valores-list li {
-                margin-bottom: 8px;
-                line-height: 1.6;
-            }
-        </style>
+    .valores-list li {
+        margin-bottom: 8px;
+        line-height: 1.6;
+    }
 
-        <section class="bg-blue">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 animate fadeInLeft">
-                        <div class="height-10"></div>
+    /* Espaçamentos padronizados */
+    .height-10 {
+        margin-bottom: 10px;
+    }
 
-                        <p class="text-justify">A Techlog Serviços de Gestão e Sistemas Informatizados LTDA. é uma empresa com ampla experiência em consultoria técnico-organizacional e gestão de mão de obra terceirizada. Desde 2000, tem participado de importantes projetos de informática aplicada na região Norte, atuando diretamente ou em parceria com instituições públicas e privadas. Destaca-se como integradora de talentos e soluções, promovendo inovação, pesquisa aplicada e desenvolvimento de tecnologias voltadas ao cotidiano da população.</p>
+    .height-20 {
+        margin-bottom: 20px;
+    }
 
-                        <div class="height-10"></div>
+    .height-40 {
+        margin-bottom: 40px;
+    }
 
-                        <div class="height-20"></div>
-                        <a href="/fale-conosco" class="btn" style="background-color: #e77242; border-color: #d05e2a; color: #fff;" data-text="Saiba mais">Saiba mais</a>
-                        <div class="height-40"></div>
-                    </div>
-                </div>
+    /* Botões padronizados */
+    .btn-techlog {
+        background-color: #e77242;
+        border-color: #d05e2a;
+        color: #fff;
+    }
+
+    .btn-techlog:hover {
+        background-color: #d05e2a;
+        color: #fff;
+    }
+</style>
+
+<section class="bg-blue">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 animate fadeInLeft">
+                <div class="height-10"></div>
+                <p class="text-justify">A Techlog Serviços de Gestão e Sistemas Informatizados LTDA. é uma empresa com ampla experiência em consultoria técnico-organizacional e gestão de mão de obra terceirizada. Desde 2000, tem participado de importantes projetos de informática aplicada na região Norte, atuando diretamente ou em parceria com instituições públicas e privadas. Destaca-se como integradora de talentos e soluções, promovendo inovação, pesquisa aplicada e desenvolvimento de tecnologias voltadas ao cotidiano da população.</p>
+                <div class="height-20"></div>
+                <a href="/fale-conosco" class="btn btn-techlog" data-text="Saiba mais">Saiba mais</a>
+                <div class="height-40"></div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
 
-        <section class="different-services text-center">
-            <div class="container">
-                <div class="heading animate bounceIn">
-                    <h1 class="color-white">Valores</h1>
+<section class="different-services text-center">
+    <div class="container">
+        <div class="heading animate bounceIn">
+            <h1 class="color-white">Valores</h1>
+        </div>
+    </div>
+</section>
+
+<section class="bg-blue">
+    <div class="container">
+        <div class="services text-center">
+            <div class="three-items-carousel owl-carousel">
+                <div class="service-box animate fadeInUp">
+                    <i class="icon-img-1"></i>
+                    <h4>Missão</h4>
+                    <p>Democratizar o acesso ao conhecimento e impulsionar a educação em larga escala, desenvolvendo e fornecendo soluções tecnológicas inovadoras e serviços técnicos especializados na produção de conteúdo educacional e comunicação corporativa.</p>
                 </div>
-            </div>
-        </section>
-
-        <section class="bg-blue">
-            <div class="container">
-                <div class="services text-center">
-                    <div class="three-items-carousel owl-carousel">
-                        <div class="service-box animate fadeInUp">
-                            <i class="icon-img-1"></i>
-                            <h4>Missão</h4>
-                            <p>Democratizar o acesso ao conhecimento e impulsionar a educação em larga escala, desenvolvendo e fornecendo soluções tecnológicas inovadoras e serviços técnicos especializados na produção de conteúdo educacional e comunicação corporativa.</p>
-                        </div>
-                        <div class="service-box">
-                            <i class="icon-img-2"></i>
-                            <h4>Visão</h4>
-                            <p>Ser referência em tecnologia educacional, reconhecida pela excelência, inovação e capacidade de adaptação às demandas do mercado, transformando a maneira como o conhecimento é acessado e disseminado, e contribuindo para o sucesso e satisfação de nossos parceiros.</p>
-                        </div>
-                        <div class="service-box">
-                            <i class="icon-img-3"></i>
-                            <h4>Valores</h4>
-                            <ul class="valores-list"></ul>
-                            <li><strong>Integridade:</strong> Atuar com ética, transparência e responsabilidade.</li>
-                            <li><strong>Inovação:</strong> Desenvolver soluções tecnológicas avançadas e adaptáveis.</li>
-                            <li><strong>Excelência:</strong> Garantir a qualidade e eficácia dos serviços e produtos.</li>
-                            </ul>
-                        </div>
-                    </div>
+                <div class="service-box animate fadeInUp">
+                    <i class="icon-img-2"></i>
+                    <h4>Visão</h4>
+                    <p>Ser referência em tecnologia educacional, reconhecida pela excelência, inovação e capacidade de adaptação às demandas do mercado, transformando a maneira como o conhecimento é acessado e disseminado, e contribuindo para o sucesso e satisfação de nossos parceiros.</p>
                 </div>
-            </div>
-        </section>
-
-
-        <div class="contact-us-bar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-9">
-                        <h4 class="animate fadeInLeft text-center">Quer nos contactar? Clique ao lado</h4>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="text-left">
-                            <a href="/fale-conosco" class="btn get-in-touch animate fadeInRight" style="background-color: #e77242; border-color: #d05e2a; color: #fff;" data-text="Fale Conosco"><i class="icon-telephone114"></i>Fale Conosco</a>
-                        </div>
-                    </div>
+                <div class="service-box animate fadeInUp">
+                    <i class="icon-img-3"></i>
+                    <h4>Valores</h4>
+                    <ul class="valores-list">
+                        <li><strong>Integridade:</strong> Atuar com ética, transparência e responsabilidade.</li>
+                        <li><strong>Inovação:</strong> Desenvolver soluções tecnológicas avançadas e adaptáveis.</li>
+                        <li><strong>Excelência:</strong> Garantir a qualidade e eficácia dos serviços e produtos.</li>
+                    </ul>
                 </div>
             </div>
         </div>
+    </div>
+</section>
+
+<div class="contact-us-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <h4 class="animate fadeInLeft text-center">Quer nos contactar? Clique ao lado</h4>
+            </div>
+            <div class="col-md-3">
+                <div class="text-left">
+                    <a href="/fale-conosco" class="btn btn-techlog get-in-touch animate fadeInRight" data-text="Fale Conosco"><i class="icon-telephone114"></i>Fale Conosco</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
